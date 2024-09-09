baa9d3fa7d5e7f7e36de7fa57c5b7a3d319fc09c
Thom Martin <3panman@users.noreply.github.com>
2024-09-09 13:26:00 -0700

    Update JamfConnectResetAuthDB.sh
    Moved login window check/fix into "Has Jamf Connect" section so in case the script has to install JC, it won't then check to see which login window the Mac is using as a fresh install should be using the JC login.

--- JamfConnectResetAuthDB.sh
+++ JamfConnectResetAuthDB.sh
@@ -6,31 +6,29 @@ currentUser=$(stat -f "%Su" /dev/console)
 loginwindow_check=$(security authorizationdb read system.login.console | grep 'loginwindow:login' 2>&1 > /dev/null; echo $?)
 
 # Is Jamf Connect Installed?
-if [ -e "/Library/LaunchAgents/com.jamf.connect.plist" ]
-then
-	# Yes. Move along.
+if [ -e "/Library/LaunchAgents/com.jamf.connect.plist" ] then
+	# JC is installed. Move along.
 	echo "Has Jamf Connect"
-else
-	# No. Install it.
-	echo "Missing Jamf Connect"
-	/usr/local/bin/jamf policy -event InstallJamfConnect
-fi
-
-if [ $loginwindow_check == 0 ]; then
-	# Mac has reverted to macOS login screen, needs fixing.
-	echo "macOS Login Screen, resetting authdb"
-	/usr/local/bin/authchanger -reset -JamfConnect
-	# If someone is logged in, exit.
-	if [[ $currentUser != root ]]; then
-		echo "A user is logged in, exiting without reboot."
-		exit 0
+	if [ $loginwindow_check == 0 ]; then
+		# Mac has reverted to macOS login screen, needs fixing.
+		echo "macOS Login Screen, resetting authdb"
+		/usr/local/bin/authchanger -reset -JamfConnect
+		# If someone is logged in, exit.
+		if [[ $currentUser != root ]]; then
+			echo "A user is logged in, exiting without reboot."
+			exit 0
+		else
+			# If nobody is logged in, reboot.
+			echo "No user is logged in, rebooting"
+			shutdown -r now
+		fi
 	else
-	# If nobody is logged in, reboot.
-		echo "No user is logged in, rebooting"
-		shutdown -r now
+		# Mac is still using Jamf Connect, nothing to do here.
+		echo "Jamf Connect Login Screen, exiting."
+		exit 0
 	fi
 else
-	# Mac is still using Jamf Connect, nothing to do here.
-	echo "Jamf Connect Login Screen, exiting."
-	exit 0
+	# JC not isntalled. Install it.
+	echo "Missing Jamf Connect"
+	/usr/local/bin/jamf policy -event InstallJamfConnect
 fi
