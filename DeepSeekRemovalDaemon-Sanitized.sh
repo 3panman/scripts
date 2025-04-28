f29d7a109d84068896c6ecc72e6bafb13e3a796c
Thom Martin <3panman@users.noreply.github.com>
2025-04-28 13:52:33 -0700

    Update DeepSeekRemovalDaemon-Sanitized.sh

--- DeepSeekRemovalDaemon-Sanitized.sh
+++ DeepSeekRemovalDaemon-Sanitized.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-daemonPath="/Library/LaunchDaemons/com.AnyOrg.dscleanup.plist"
+# Replace "AnyOrg" with your org identifier
+daemonPath="/Library/LaunchDaemons/com.AnyOrg.dscleanup"
 
 if [[ -f "$daemonPath" ]]; then
 	echo "Daemon already present"
 	exit
 else
-cat > "$daemonPath" << EOF
+	cat > "$daemonPath" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-	<dict>
-		<key>Label</key>
-		<string>com.AnyOrg.dscleanup</string>
-		<key>ProgramArguments</key>
-		<array>
-			<string>sh</string>
-			<string>-c</string>
-			<string>/usr/local/bin/jamf policy -event KillDeepSeek</string>
-		</array>
-		<key>StartInterval</key>
-		<integer>259200</integer>
-	</dict>
+		<dict>
+				<key>Label</key>
+				<string>com.AnyOrg.dscleanup</string>
+				<key>ProgramArguments</key>
+				<array>
+						<string>sh</string>
+						<string>-c</string>
+						<string>/usr/local/bin/jamf policy -event KillDeepSeek</string>
+				</array>
+				<key>StartInterval</key>
+				<integer>259200</integer>
+		</dict>
 </plist>
 EOF
 
-/bin/chmod 644 "$daemonPath"
-/usr/sbin/chown root:wheel "$daemonPath"
-sudo /bin/launchctl bootstrap system "$daemonPath"
+	/bin/chmod 644 "$daemonPath".plist
+	/usr/sbin/chown root:wheel "$daemonPath".plist
+	sudo /bin/launchctl bootstrap system "$daemonPath".plist
 fi
